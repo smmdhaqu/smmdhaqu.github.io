81c91a0be80e3c79b3af2f1071dca9728ec27ebf
Shamual <shraju.db@gmail.com>
2024-12-28 12:31:45 +0600

    minor changes

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Shamsul Haque";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -45,7 +45,6 @@ function showProjects(projects) {
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
-        <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
       <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
